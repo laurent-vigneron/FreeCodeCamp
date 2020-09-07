56dd23fd1e32bb4776328cb5c48dd5c66a3100f9
Laurent Vigneron <laurent-vigneron@users.noreply.github.com>
2020-09-07 21:50:37 +0800

    Unit Tests done and passing for the Sudoku Solver (Quality Assurance Track)

--- 6-Quality_Assurance_Certification/4-Sudoku_Solver/public/sudoku-solver.js
+++ 6-Quality_Assurance_Certification/4-Sudoku_Solver/public/sudoku-solver.js
@@ -1,32 +1,46 @@
-const textArea = document.getElementById('text-input');
-const grid = document.querySelectorAll('input[type="text"]');
-const error = document.getElementById('error-msg');
-const solveBtn = document.getElementById('solve-button');
-const clearBtn = document.getElementById('clear-button');
 
-import { puzzlesAndSolutions } from './puzzle-strings.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  init();
+  gridUpdate();
+})
+
+function validInput(digit) {
+  if(digit===null) {
+    return '.';
+  } else if (digit.toString().match(/[1-9]/) && digit.toString().length === 1) {
+    return digit;
+  } else return '.';
+}
+
+function addDigit(digit, index) {
+  const textArea = document.getElementById('text-input');
+  const error = document.getElementById('error-msg');
+  textArea.value=textArea.value.slice(0,index) + validInput(digit) + textArea.value.slice(index+1);
+  error.innerHTML = '';
+}
+
+function init(value = '') {
+  const textArea = document.getElementById('text-input');
+  const grid = document.querySelectorAll('input[type="text"]');
+  const error = document.getElementById('error-msg');
+  const solveBtn = document.getElementById('solve-button');
+  const clearBtn = document.getElementById('clear-button');
 
   // Load a simple puzzle into the text area
-  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  if(value==='') {
+    textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  } else {
+    textArea.value = value;
+  }
 
   grid.forEach((cell, i) => {
-    cell.addEventListener('input', (e) => {
-      if(e.data===null) { 
-        textArea.value=textArea.value.slice(0,i) + '.' + textArea.value.slice(i+1);
-      } else if (!isNaN(e.data)) {
-        textArea.value=textArea.value.slice(0,i) + e.data + textArea.value.slice(i+1);
-      }
-      error.innerHTML = '';
-    })
+    cell.addEventListener('input', (e) => { addDigit(e.data, i); })
   })
 
   textArea.addEventListener('input', (e) => {
     gridUpdate();
   })
-
-  gridUpdate();
 
   clearBtn.addEventListener('click', () => {
     textArea.value = '.'.repeat(81);
@@ -55,9 +69,14 @@ document.addEventListener('DOMContentLoaded', () => {
       error.innerHTML = 'No solution has been found for this problem.'
     }
   })
-});
+}
 
 function gridUpdate() {
+
+  const textArea = document.getElementById('text-input');
+  const grid = document.querySelectorAll('input[type="text"]');
+  const error = document.getElementById('error-msg');
+
   if(textArea.value.length === 81) {
     error.innerHTML = '';
     grid.forEach((cell, i) => {
@@ -72,13 +91,43 @@ function gridUpdate() {
   }
 }
 
+const puzzlesAndSolutions = [
+  [
+    '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+    '135762984946381257728459613694517832812936745357824196473298561581673429269145378'
+  ],
+  [
+    '5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3',
+    '568913724342687519197254386685479231219538467734162895926345178473891652851726943'
+  ],
+  [
+    '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1',
+    '218396745753284196496157832531672984649831257827549613962415378185763429374928561'
+  ],
+  [
+    '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6',
+    '473891265851726394926345817568913472342687951197254638734162589685479123219538746'
+  ],
+  [
+    '82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51',
+    '827549163531672894649831527496157382218396475753284916962415738185763249374928651'
+  ],
+  [
+    '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..',
+    '769235418851496372432178956174569283395842761628713549283657194516924837947381625'
+  ]
+];
+
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
 try {
-  module.exports = {
-    
-  }
+  module.exports.init = init;
+  module.exports.gridUpdate = gridUpdate;
+  module.exports.addDigit = addDigit;
+  module.exports.validInput = validInput; 
+  module.exports.puzzlesAndSolutions = puzzlesAndSolutions;
 } catch (e) {}
--- 6-Quality_Assurance_Certification/4-Sudoku_Solver/tests/1_unit-tests.js
+++ 6-Quality_Assurance_Certification/4-Sudoku_Solver/tests/1_unit-tests.js
@@ -20,34 +20,43 @@ suite('UnitTests', () => {
       .then((dom) => {
         global.window = dom.window;
         global.document = dom.window.document;
-
         Solver = require('../public/sudoku-solver.js');
       });
   });
   
   // Only the digits 1-9 are accepted
   // as valid input for the puzzle grid
-  suite('Function ____()', () => {
+  suite('Function validInput()', () => {
     test('Valid "1-9" characters', (done) => {
       const input = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-      // done();
+      input.forEach((num) => { 
+        let cell = Solver.validInput(num);
+        // console.log(num, cell);
+        assert.equal(cell, num);
+      })
+      done();
     });
 
     // Invalid characters or numbers are not accepted 
     // as valid input for the puzzle grid
     test('Invalid characters (anything other than "1-9") are not accepted', (done) => {
       const input = ['!', 'a', '/', '+', '-', '0', '10', 0, '.'];
-
-      // done();
+      input.forEach((char) => { 
+        let cell = Solver.validInput(char);
+        // console.log(char, cell);
+        assert.equal(cell, '.');
+      })
+      done();
     });
   });
   
-  suite('Function ____()', () => {
+  suite('Function init()', () => {
     test('Parses a valid puzzle string into an object', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
+      const textArea = document.getElementById('text-input');
+      Solver.init(input);
+      assert.equal(textArea.value, input);
+      done();
     });
     
     // Puzzles that are not 81 numbers/periods long show the message 
@@ -58,8 +67,13 @@ suite('UnitTests', () => {
       const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
       const errorMsg = 'Error: Expected puzzle to be 81 characters long.';
       const errorDiv = document.getElementById('error-msg');
-      
-      // done();
+      Solver.init(shortStr);
+      Solver.gridUpdate();
+      assert.equal(errorDiv.innerHTML, errorMsg);
+      Solver.init(longStr);
+      Solver.gridUpdate();
+      assert.equal(errorDiv.innerHTML, errorMsg);
+      done();
     });
   });
 
@@ -67,15 +81,25 @@ suite('UnitTests', () => {
     // Valid complete puzzles pass
     test('Valid puzzles pass', done => {
       const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+      const errorMsg = 'Solution found, grid updated accordingly.';
+      const errorDiv = document.getElementById('error-msg');
+      Solver.init(input);
+      Solver.gridUpdate();
+      document.getElementById('solve-button').click();
+      assert.equal(errorDiv.innerHTML, errorMsg);
+      done();
     });
 
     // Invalid complete puzzles fail
     test('Invalid puzzles fail', done => {
       const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+      const errorMsg = 'No solution has been found for this problem.';
+      const errorDiv = document.getElementById('error-msg');
+      Solver.init(input);
+      Solver.gridUpdate();
+      document.getElementById('solve-button').click();
+      assert.equal(errorDiv.innerHTML, errorMsg);
+      done();
     });
   });
   
@@ -84,8 +108,16 @@ suite('UnitTests', () => {
     // Returns the expected solution for a valid, incomplete puzzle
     test('Returns the expected solution for an incomplete puzzle', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
+      const solution = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
+      const errorMsg = 'Solution found, grid updated accordingly.';
+      const textArea = document.getElementById('text-input');
+      const errorDiv = document.getElementById('error-msg');
+      Solver.init(input);
+      Solver.gridUpdate();
+      document.getElementById('solve-button').click();
+      assert.equal(errorDiv.innerHTML, errorMsg);
+      assert.equal(textArea.value, solution);
+      done();
     });
   });
 });
