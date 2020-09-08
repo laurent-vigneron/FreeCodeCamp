d1ef7d4d4001a9477ac1a82198914280af0c2b6a
Laurent Vigneron <laurent-vigneron@users.noreply.github.com>
2020-09-08 23:40:47 +0800

    Logic and Unit Test done for the US-UK Translator (Quality Assurance Certification)

--- 6-Quality_Assurance_Certification/5-American_British_Translator/public/translator.js
+++ 6-Quality_Assurance_Certification/5-American_British_Translator/public/translator.js
@@ -3,13 +3,162 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+})
+
+function init() {
+  const screen = {
+      textInput: document.getElementById('text-input'),
+      language: document.getElementById('locale-select'),
+      translation: document.getElementById('translated-sentence'),
+      error: document.getElementById('error-msg'),
+      translateBtn: document.getElementById('translate-btn'),
+      clearBtn: document.getElementById('clear-btn')
+  }
+  
+  screen.clearBtn.addEventListener('click', () => {
+    screen.textInput.value = '';
+    screen.translation.innerHTML = '';
+    screen.error.innerHTML = '';
+  })
+
+  screen.translateBtn.addEventListener('click', () => { translate(screen) })
+}
+
+function translate(screen) {
+  let result;
+  if(screen.language.value==='american-to-british'){
+    result = usToEn(screen.textInput.value);
+  } else {
+    result = enToUs(screen.textInput.value);
+  }
+  screen.error.innerHTML = (result==='') ? 'Error: No text to translate.' : '';
+  screen.translation.innerHTML = result;
+}
+
+function usToEn(string) {
+  let result = string;
+  let reg;
+
+  if(result==='') return '';
+
+  let save = '';
+  // British only
+  Object.keys(americanOnly).forEach(term => {
+    const regex =  new RegExp('^'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'$','ig'); 
+    let temp = result.match(regex) || [''];
+    if(temp[0].length>save.length) {
+      save = term;
+      reg = regex;
+    }
+  })
+  result = result.replace(reg, `<span class="highlight">${americanOnly[save]}</span>`);
+
+  save = '';
+  // the rest
+  Object.keys(americanToBritishSpelling).forEach(term => {
+    const regex =  new RegExp('^'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'$','ig'); 
+    let temp = result.match(regex) || [''];
+    // console.log(temp, term);
+    if(temp[0].length>save.length) {
+      save = term;
+      reg = regex;
+    }
+  })
+  result = result.replace(reg, `<span class="highlight">${americanToBritishSpelling[save]}</span>`);
+
+  Object.keys(americanToBritishTitles).forEach(term => {
+    const regex =  new RegExp(term+' ','ig'); 
+    const reference = result.match(regex) || term;
+    // console.log(reference);
+    result = result.replace(regex, `<span class="highlight">${matchCase(americanToBritishTitles[term]+'', reference[0].slice(0,-2))} </span>`);
+  })
+
+  // take care of time
+  result = result.replace(/(\W\d?\d)[:](\d\d)/, '<span class="highlight">$1.$2</span>');
+
+  if(result==string) {
+    result = 'Everything looks good to me!';
+  }
+
+  return result;
+}
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
+
+function matchCase(s1, s2) {
+  let result = '';
+  // console.log(s1, s1.length, s2, s2.length);
+  for(let i=0; i<s2.length; i++){
+    if(s2[i]===s2[i].toUpperCase()){
+      result += s1[i].toUpperCase();
+    } else {
+      result += s1[i];
+    }
+  }
+  return result;
+}
+
+function enToUs(string) {
+  let result = string;
+  let reg; // new RegExp(`(^${term}\\W)|(\\W${term}\\W)|(\\W${term}$)`,'ig');
+
+  let save = '';
+  // remove what is UK only
+  for(let i=0; i<string.split(' ').length; i++) {
+    Object.keys(britishOnly).forEach(term => {
+    const regex =  new RegExp('^'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'$','ig'); 
+      let temp = result.match(regex) || [''];
+      // console.log(temp);
+      if(temp[0].length>save.length) {
+        save = term;
+        reg = regex;
+      }
+      // console.log(save);
+    })
+    result = result.replace(reg, `<span class="highlight">${britishOnly[save]}</span>`);
+
+    save = '';
+    // the rest
+    Object.values(americanToBritishSpelling).forEach(term => {
+      const regex =  new RegExp('^'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'(?=\\W)(?!-)|(?<=\\W)(?<!-)'+term+'$','ig'); 
+      let temp = result.match(regex) || [''];
+      // console.log(temp);
+      if(temp[0].length>save.length) {
+        save = term;
+        reg = regex;
+      }
+    })
+    result = result.replace(reg, `<span class="highlight">${getKeyByValue(americanToBritishSpelling ,save)}</span>`);
+
+    Object.values(americanToBritishTitles).forEach(term => {
+      const regex =  new RegExp(term+' ','ig'); 
+      const reference = result.match(regex) || term;
+      // console.log(reference);
+      result = result.replace(regex, `<span class="highlight">${matchCase(getKeyByValue(americanToBritishTitles, term), reference[0])} </span>`);
+    })
+  }
+
+  // take care of time
+  result = result.replace(/(\W\d?\d)[.](\d\d)/, '<span class="highlight">$1:$2</span>');
+
+  if(result==string) {
+    result = 'Everything looks good to me!';
+  }
+  return result;
+}
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
 try {
-  module.exports = {
-
-  }
+  module.exports.init = init,
+  module.exports.translate = translate,
+  module.exports.usToEn = usToEn,
+  module.exports.enToUs = enToUs
 } catch (e) {}
--- 6-Quality_Assurance_Certification/5-American_British_Translator/tests/1_unit-tests.js
+++ 6-Quality_Assurance_Certification/5-American_British_Translator/tests/1_unit-tests.js
@@ -24,78 +24,100 @@ suite('Unit Tests', () => {
       });
   });
 
-  suite('Function ____()', () => {
+  suite('Function usToEn()', () => {
 
     suite('American to British English', () => {
 
       test('Mangoes are my favorite fruit. --> Mangoes are my favourite fruit.', done => {
         const input = 'Mangoes are my favorite fruit.';
-        const output = 'Mangoes are my favourite fruit.';
-
-        // done();
+        const output = 'Mangoes are my <span class="highlight">favourite</span> fruit.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        console.log(output);
+        assert.equal(result, output);
+        done();
       });
 
       test('I ate yogurt for breakfast. --> I ate yoghurt for breakfast.', done => {
         const input = 'I ate yogurt for breakfast.';
-        const output = 'I ate yoghurt for breakfast.';
+        const output = 'I ate <span class="highlight">yoghurt</span> for breakfast.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
 
-        // done();
       });
 
       test("We had a party at my friend's condo. --> We had a party at my friend's flat.", done => {
         const input = "We had a party at my friend's condo.";
-        const output = "We had a party at my friend's flat.";
-
-        // done();
+        const output = `We had a party at my friend's <span class="highlight">flat</span>.`;
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Can you toss this in the trashcan for me? --> Can you toss this in the bin for me?', done => {
         const input = 'Can you toss this in the trashcan for me?';
-        const output = 'Can you toss this in the bin for me?';
-
-        // done();
+        const output = 'Can you toss this in the <span class="highlight">bin</span> for me?';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('The parking lot was full. --> The car park was full.', done => {
         const input = 'The parking lot was full.';
-        const output = 'The car park was full.';
-
-        // done();
+        const output = 'The <span class="highlight">car park</span> was full.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Like a high tech Rube Goldberg machine. --> Like a high tech Heath Robinson device.', done => {
         const input = 'Like a high tech Rube Goldberg machine.';
-        const output = 'Like a high tech Heath Robinson device.';
-
-        // done();
+        const output = 'Like a high tech <span class="highlight">Heath Robinson device</span>.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
       
       test('To play hooky means to skip class or work. --> To bunk off means to skip class or work.', done => {
         const input = 'To play hooky means to skip class or work.';
-        const output = 'To bunk off means to skip class or work.';
-
-        // done();
+        const output = 'To <span class="highlight">bunk off</span> means to skip class or work.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('No Mr. Bond, I expect you to die. --> No Mr Bond, I expect you to die. ', done => {
         const input = 'No Mr. Bond, I expect you to die.';
-        const output = 'No Mr Bond, I expect you to die.';
-
-        // done();
+        const output = 'No <span class="highlight">Mr </span>Bond, I expect you to die.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Dr. Grosh will see you now. --> Dr Grosh will see you now. ', done => {
         const input = 'Dr. Grosh will see you now.';
-        const output = 'Dr Grosh will see you now.';
-
-        // done();
+        const output = '<span class="highlight">Dr </span>Grosh will see you now.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Lunch is at 12:15 today. --> Lunch is at 12.15 today.', done => {
         const input = 'Lunch is at 12:15 today.';
-        const output = 'Lunch is at 12.15 today.';
-        
-        // done();
+        const output = 'Lunch is at<span class="highlight"> 12.15</span> today.';
+        let result = Translator.usToEn(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
     });
@@ -104,72 +126,92 @@ suite('Unit Tests', () => {
 
       test('We watched the footie match for a while. --> We watched the soccer match for a while.', done => {
         const input = 'We watched the footie match for a while.';
-        const output = 'We watched the soccer match for a while.';
-
-        // done();
+        const output = 'We watched the <span class="highlight">soccer</span> match for a while.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Paracetamol takes up to an hour to work. --> Tylenol takes up to an hour to work.', done => {
         const input = 'Paracetamol takes up to an hour to work.';
-        const output = 'Tylenol takes up to an hour to work.';
-
-        // done();
+        const output = '<span class="highlight">Tylenol</span> takes up to an hour to work.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('First, caramelise the onions. --> First, caramelize the onions.', done => {
         const input = 'First, caramelise the onions.';
-        const output = 'First, caramelize the onions.';
-
-        // done();
+        const output = 'First, <span class="highlight">caramelize</span> the onions.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('I spent the bank holiday at the funfair. --> I spent the public holiday at the carnival.', done => {
         const input = 'I spent the bank holiday at the funfair.';
-        const output = 'I spent the public holiday at the carnival.';
-
-        // done();
+        const output = 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('I had a bicky then went to the chippy. --> I had a cookie then went to the fish-and-chip shop.', done => {
         const input = 'I had a bicky then went to the chippy.';
-        const output = 'I had a cookie then went to the fish-and-chip shop.';
-
-        // done();
+        const output = 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test("I've just got bits and bobs in my bum bag. --> I've just got odds and ends in my fanny pack.", done => {
         const input = "I've just got bits and bobs in my bum bag.";
-        const output = "I've just got odds and ends in my fanny pack.";
-
-        // done();
+        const output = `I've just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.`;
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
       
       test("The car boot sale at Boxted Airfield was called off. --> The swap meet at Boxted Airfield was called off.", done => {
         const input = "The car boot sale at Boxted Airfield was called off.";
-        const output = "The swap meet at Boxted Airfield was called off.";
-
-        // done();
+        const output = 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test("Have you met Mrs Kalyani? --> Have you met Mrs. Kalyani?", done => {
         const input = "Have you met Mrs Kalyani?";
-        const output = "Have you met Mrs. Kalyani?";
-
-        // done();
+        const output = 'Have you met <span class="highlight">Mrs. </span>Kalyani?';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test("Prof Joyner of King's College, London. --> Prof. Joyner of King's College, London.", done => {
         const input = "Prof Joyner of King's College, London.";
-        const output = "Prof. Joyner of King's College, London.";
-
-        // done();
+        const output = `<span class="highlight">Prof. </span>Joyner of King's College, London.`;
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
       test('Tea time is usually around 4 or 4.30. --> Tea time is usually around 4 or 4:30.', done => {
-        const input = 'Lunch is at 12:15 today.';
-        const output = 'Lunch is at 12.15 today.';
-
-        // done();
+        const input = 'Tea time is usually around 4 or 4.30.';
+        const output = 'Tea time is usually around 4 or<span class="highlight"> 4:30</span>.';
+        let result = Translator.enToUs(input);
+        console.log(result);
+        assert.equal(result, output);
+        done();
       });
 
     });
