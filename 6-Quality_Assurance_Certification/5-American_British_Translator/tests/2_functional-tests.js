9a155726d203bb9a781ceabc7e86286de46e8bc3
Laurent Vigneron <laurent-vigneron@users.noreply.github.com>
2020-09-09 10:56:03 +0800

    Functional Tests Done, project is over

--- 6-Quality_Assurance_Certification/5-American_British_Translator/tests/2_functional-tests.js
+++ 6-Quality_Assurance_Certification/5-American_British_Translator/tests/2_functional-tests.js
@@ -8,6 +8,12 @@
 
 const chai = require('chai');
 const assert = chai.assert;
+let textInput;
+let language;
+let translation; 
+let error;
+let translateBtn;
+let clearBtn;
 
 let Translator;
 
@@ -15,6 +21,12 @@ suite('Functional Tests', () => {
   suiteSetup(() => {
     // DOM already mocked -- load translator then run tests
     Translator = require('../public/translator.js');
+    textInput = document.getElementById('text-input');
+    language = document.getElementById('locale-select');
+    translation = document.getElementById('translated-sentence');
+    error = document.getElementById('error-msg');
+    translateBtn = document.getElementById('translate-btn');
+    clearBtn = document.getElementById('clear-btn');
   });
 
   suite('Function ____()', () => {
@@ -24,8 +36,13 @@ suite('Functional Tests', () => {
       `<span class="highlight">...</span>` tags when the "Translate" button is pressed.
     */
     test("Translation appended to the `translated-sentence` `div`", done => {
-
-      // done();
+      const input = 'Mangoes are my favorite fruit.';
+      const output = 'Mangoes are my <span class="highlight">favourite</span> fruit.';
+      textInput.value = input;
+      Translator.init();
+      translateBtn.click();
+      assert.equal(translation.innerHTML, output);
+      done();
     });
 
     /* 
@@ -34,8 +51,13 @@ suite('Functional Tests', () => {
       `translated-sentence` `div` when the "Translate" button is pressed.
     */
     test("'Everything looks good to me!' message appended to the `translated-sentence` `div`", done => {
-
-      // done();
+      const input = 'Mangoes are a fruit.';
+      const output = 'Everything looks good to me!';
+      textInput.value = input;
+      Translator.init();
+      translateBtn.click();
+      assert.equal(translation.innerHTML, output);
+      done();
     });
 
     /* 
@@ -44,8 +66,13 @@ suite('Functional Tests', () => {
       the `error-msg` `div`.
     */
     test("'Error: No text to translate.' message appended to the `translated-sentence` `div`", done => {
-
-      // done();
+      const input = '';
+      const output = 'Error: No text to translate.';
+      textInput.value = input;
+      Translator.init();
+      translateBtn.click();
+      assert.equal(error.innerHTML, output);
+      done();
     });
 
   });
@@ -56,8 +83,18 @@ suite('Functional Tests', () => {
       `divs` are cleared when the "Clear" button is pressed.
     */
     test("Text area, `translated-sentence`, and `error-msg` are cleared", done => {
-
-      // done();
+      textInput.value = '';
+      Translator.init();
+      translateBtn.click();
+      assert.equal(error.innerHTML, 'Error: No text to translate.');
+      clearBtn.click();
+      assert.equal(error.innerHTML, '');
+      textInput.value = 'Mangoes are a fruit.';
+      translateBtn.click();
+      assert.equal(translation.innerHTML, 'Everything looks good to me!');
+      clearBtn.click();
+      assert.equal(translation.innerHTML, '');
+      done();
     });
 
   });
