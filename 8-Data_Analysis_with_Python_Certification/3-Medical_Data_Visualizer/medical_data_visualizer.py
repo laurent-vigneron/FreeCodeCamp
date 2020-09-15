e3821a86c841d0483c41f0603d520beb7e490f17
Laurent Vigneron <laurent-vigneron@users.noreply.github.com>
2020-09-15 11:55:53 +0800

    Third project of the Data-Analysis with Python path. First graph done.

--- 8-Data_Analysis_with_Python_Certification/3-Medical_Data_Visualizer/medical_data_visualizer.py
+++ 8-Data_Analysis_with_Python_Certification/3-Medical_Data_Visualizer/medical_data_visualizer.py
@@ -4,26 +4,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('./medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = df['weight'] / ((df['height']/100)**2) > 25
+df['overweight'] = df['overweight'].astype(np.int8)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df['cholesterol'] = df['cholesterol'] > 1 
+df['gluc'] = df['gluc'] > 1
+df['cholesterol'] = df['cholesterol'].astype(np.int8) 
+df['gluc'] = df['gluc'].astype(np.int8)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df, id_vars = ['cardio'], value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
-    df_cat = None
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    # df_cat = df_cat.groupby('cardio').groups
+    df_cat = df_cat.groupby(['cardio','variable','value'], as_index = False).size() 
 
     # Draw the catplot with 'sns.catplot()'
-
-
+    fig = sns.catplot(x="variable", y='size', hue='value', kind='bar', col="cardio", data=df_cat)
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
